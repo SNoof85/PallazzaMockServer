f38b2525c0eb7f8a63900f00f88eaf1cf073329f
marvingrieger <mail@marvingrieger.de>
2018-06-03 14:31:25 +0200

    Updated behaviour: a new nonce is only generated if a post was performed. Removed some dynamics which did not correspond to the actual behavior of the device.

--- controller/statusGenerator.js
+++ controller/statusGenerator.js
@@ -13,6 +13,7 @@ module.exports = {
     setState: function (secret, body) {
         if (!validateSecret(secret))
         {
+            newNonceRequired = true;
             return false;
         }
         else
@@ -23,11 +24,13 @@ module.exports = {
             {
                 logger.debug("Applying new temperature");
                 sp_temp = command['sp_temp'];
+                newNonceRequired = true;
             }
             else if (command['prg'] !== undefined)
             {
                 logger.debug("Applying new program");
                 prg = command['prg'];
+                newNonceRequired = true;
             }
         }
     }
@@ -44,6 +47,7 @@ let sp_temp = 21;
 let prg = false;
 // Current nonce
 let currentNonce;
+let newNonceRequired = true;
 
 // Status, if the stove is "Off"
 const defaultStatusOff = {"meta":{"sw_version":"V6.02","hw_version":"KS01","bootl_version":"V1.1","wifi_sw_version":"V1.0.1","wifi_bootl_version":"V1.0.1","sn":"0000042","typ":"HSP-6","language":"de","nonce":"mClaM2hQINs64uW3","wlan_features":["l", "hl", "wp"]},"prg":false,"wprg":false,"mode":"off","sp_temp":42,"is_temp":18.94,"ht_char":4,"weekprogram":[{"day":"we","begin":"00:00","end":"24:00","temp":21}],"error":[],"eco_mode":true,"pgi":false,"ignitions":14,"on_time":23,"consumption":28,"maintenance_in":972,"cleaning_in":1686};
@@ -76,14 +80,19 @@ function assembleStatusString() {
     }
 
     // Global corrections
-    // Set random nonce
-    currentNonce = generateNonce();
+    // Do we need a new nonce?
+    if (newNonceRequired)
+    {
+        currentNonce = generateNonce();
+        newNonceRequired = false;
+    }
+
+    // Set nonce
     status['meta']['nonce'] = currentNonce;
     // Set temperature
     status['sp_temp'] = sp_temp;
-
-    // Update internal state
-    updateInternalState();
+    // Set prg
+    status['prg'] = prg;
 
     return status;
 }
@@ -166,7 +175,7 @@ function validateSecret(secret)
 function calculateHSPIN(NONCE, HPIN)
 {
     let result = md5(NONCE + HPIN);
-    logger.debug('HPIN: ' + HPIN);
+    logger.debug('HSPIN: ' + HPIN);
 
     return result;
 }
@@ -175,7 +184,7 @@ function calculateHSPIN(NONCE, HPIN)
 function calculateHPIN(PIN)
 {
     let result = md5(PIN);
-    logger.debug('HSPIN: ' + result);
+    logger.debug('HPIN: ' + result);
 
     return result;
 }
